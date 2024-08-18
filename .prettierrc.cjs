bc008ea02b67ae908ae5972c29524efc24b93768
jdchi <chijiaduo@hotmail.com>
2024-08-18 14:39:45 +0800

    格式化

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,7 @@
 // prettierrc 的配置文件，使用 commonJs，因此用 module.exports
 module.exports = {
-	printWidth: 20,
-	tabWidth: 10,
-	semi: false, // 不用分号
-};
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 4,
+  semi: false, // 不用分号
+}
