79a556d805ae903df423e2a75da2e75688ce4808
jdchi <chijiaduo@hotmail.com>
2024-08-25 22:55:17 +0800

    feat: 页面跳转

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,16 +4,20 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 const router = [
     {
+        path: '/',
+        element: <div>Welcomeddd</div>
+    },
+    {
         path: '/login',
-        element: Login()
+        element: <Login /> // 要注意这里的组件写法，不要写成函数调用了
     },
     {
         path: '*',
-        element: <Navigate to={'/404'}></Navigate>
+        element: <Navigate to='404' />
     },
     {
         path: '/404',
-        element: Error404()
+        element: <Error404 />
     }
 ]
 
--- src/views/404.tsx
+++ src/views/404.tsx
@@ -1,3 +1,23 @@
-export default function Error404(){
-	return <div>404</div>
+import { Button, Result } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+function Error404() {
+    const nav = useNavigate();
+
+    function handleClick(): void {
+        nav('/');
+    }
+
+    return (
+        <Result
+            status={'404'}
+            extra={
+                <Button type='primary' onClick={handleClick}>
+                    回首页
+                </Button>
+            }
+        />
+    );
 }
+
+export default Error404;
