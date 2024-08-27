f1181d218ed05a1b97ea7b118e27d9b2d08fd300
jdchi <chijiaduo@hotmail.com>
2024-08-27 23:37:29 +0800

    feat: 封装请求

---
+++ src/utils/request.ts
@@ -0,0 +1,47 @@
+import { message } from 'antd';
+import axios from 'axios';
+import { showLoading } from './loading';
+
+const instance = axios.create({
+    baseURL: '/api',
+    timeout: 8000
+});
+
+export default {
+    get(url: string, params: any) {
+        return instance.get(url, { params });
+    },
+
+    post(url: string, params: any) {
+        return instance.post(url, params);
+    }
+};
+
+// 请求拦截器
+instance.interceptors.request.use(
+    config => {
+        showLoading();
+				console.log('interceptors')
+        const token = 'ddd';
+        if (token) {
+            config.headers.Authorization = 'Token:' + token;
+        }
+        return { ...config };
+    },
+    e => {
+        return Promise.reject(e);
+    }
+);
+
+// 响应拦截器
+instance.interceptors.response.use(resp => {
+    const data = resp.data;
+    if (data.code === 500001) {
+        message.error(data.msg);
+        location.href = '/login';
+    } else if (data.code != 0) {
+        message.error(data.msg);
+        return Promise.reject(data);
+    }
+    return data.data;
+});
--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,3 +1,19 @@
+import request from '@/utils/request';
+import { useEffect } from 'react';
+
 export default function Login() {
+    useEffect(() => {
+        request
+            .get('/get_user_info', {
+                user_id: 1
+            })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(e => {
+                console.log('error: ', e);
+            });
+    }, []);
+
     return <div>Login</div>;
 }
