cfc51fe720309abf36117fc6ec377d7f0a0f8132
jdchi <chijiaduo@hotmail.com>
2024-08-31 14:57:46 +0800

    feat: 主题色处理

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
+import { ConfigProvider } from 'antd';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+    <ConfigProvider
+        theme={{
+            token: {
+                colorPrimary: '#00b96b'
+            }
+        }}
+    >
+        <App />
+    </ConfigProvider>
+);
